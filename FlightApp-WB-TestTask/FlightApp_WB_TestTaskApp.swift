7a4bff05a402b87fbe018653e8140990e342fdfe
Konstantin Kochetkov <ka4ah4uk@gmail.com>
2023-10-18 21:19:01 +0500

    Initial

---
+++ FlightApp-WB-TestTask/FlightApp_WB_TestTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlightApp_WB_TestTaskApp.swift
+//  FlightApp-WB-TestTask
+//
+//  Created by K4 on 18.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct FlightApp_WB_TestTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
